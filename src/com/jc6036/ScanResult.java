bca353366c52f087fb0c0ff893a12f31ae2ae955
Jesse <jesse@Llebac-Debian.priv>
2018-02-05 22:03:04 -0500

    Actually wrote up this class as a dataset finally.

--- src/com/jc6036/ScanResult.java
+++ src/com/jc6036/ScanResult.java
@@ -1,6 +1,54 @@
 package com.jc6036;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class ScanResult
 {
-    /* TODO: Still needs written up. Will start out by returning a Map/Dictionary/Whatever java calls it.*/
+    /*
+    *   This class contains the dataset to be returned by PortScanner, as well as helper functions for the data.
+    *
+    *   Data is intended to be retrieved and set through the getter and setter methods.
+    * */
+
+    private Map<Integer, Boolean> mPorts; // This will be the primary data returned
+    private String sTargetAddress; // Nice to include in the set
+
+    public ScanResult()
+    {
+        mPorts = new HashMap<Integer, Boolean>();
+        sTargetAddress = "";
+    }
+
+
+    /* Getters and Setters Below */
+    public void SetPorts(Map<Integer, Boolean> mPorts)
+    {
+        this.mPorts = mPorts;
+    }
+    public Map<Integer, Boolean> GetPorts()
+    {
+        return mPorts;
+    }
+
+    public void SetAddress(String sAddress)
+    {
+        this.sTargetAddress = sAddress;
+    }
+    public String GetAddress()
+    {
+        return sTargetAddress;
+    }
+
+
+    /* Helpers */
+
+    public void AddResult(Integer nPort, Boolean bOpen)
+    {
+        mPorts.put(nPort, bOpen);
+    }
+    public void ClearResults()
+    {
+        mPorts.clear();
+    }
 }
